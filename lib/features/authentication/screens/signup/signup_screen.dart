0a5271ede414cb9d0833c2786cb134de147d4e9f
Alfaz Rafai <alfazrafai01@gmail.com>
2025-09-08 09:51:33 +0530

    Completed signup screen UI Saprate signup screen widgets

--- lib/features/authentication/screens/signup/signup_screen.dart
+++ lib/features/authentication/screens/signup/signup_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:shopping_app/common/styles/CSpacingStyle.dart';
+import 'package:shopping_app/features/authentication/screens/signup/widgets/signup_form.dart';
 import 'package:shopping_app/utils/constant/colors.dart';
 import 'package:shopping_app/utils/constant/sizes.dart';
 
@@ -25,110 +26,7 @@ class SignupScreen extends StatelessWidget {
               ),
               SizedBox(height: CSizes.lg),
 
-              Form(
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        Expanded(
-                          child: TextFormField(
-                            decoration: InputDecoration(
-                              labelText: 'First Name',
-                              prefixIcon: Icon(Iconsax.user),
-                            ),
-                          ),
-                        ),
-                        SizedBox(width: CSizes.sm),
-                        Expanded(
-                          child: TextFormField(
-                            decoration: InputDecoration(
-                              labelText: 'First Name',
-                              prefixIcon: Icon(Iconsax.user),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: CSizes.sm),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        labelText: 'Last Name',
-                        prefixIcon: Icon(Iconsax.user),
-                      ),
-                    ),
-                    SizedBox(height: CSizes.sm),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        labelText: 'Username',
-                        prefixIcon: Icon(Iconsax.user_edit),
-                      ),
-                    ),
-                    SizedBox(height: CSizes.sm),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        labelText: 'Email',
-                        prefixIcon: Icon(Iconsax.direct),
-                      ),
-                    ),
-                    SizedBox(height: CSizes.sm),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        labelText: 'Phone Number',
-                        prefixIcon: Icon(Iconsax.call),
-                      ),
-                    ),
-                    SizedBox(height: CSizes.sm),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        labelText: 'First Name',
-                        prefixIcon: Icon(Iconsax.user),
-                      ),
-                    ),
-                    SizedBox(height: CSizes.sm),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        labelText: 'Password',
-                        suffixIcon: Icon(Iconsax.eye_slash),
-                        prefixIcon: Icon(Iconsax.user),
-                      ),
-                    ),
-
-                    Row(
-                      children: [
-                        SizedBox(
-                          width: 20,
-                          height: 20,
-                          child: Checkbox(value: true, onChanged: (value) {}),
-                        ),
-                        SizedBox(width: CSizes.sm),
-                        Text.rich(
-                          TextSpan(
-                            children: [
-                              TextSpan(
-                                text: 'I agree to the',
-                                style: Theme.of(context).textTheme.bodySmall,
-                              ),
-                              TextSpan(
-                                text: 'privacy',
-                                style: Theme.of(context).textTheme.bodyMedium!
-                                    .apply(
-                                      color: dark
-                                          ? CColors.white
-                                          : CColors.primary,
-                                    ),
-                              ),
-                              TextSpan(
-                                text: 'and',
-                                style: Theme.of(context).textTheme.bodySmall,
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
+              SignupForm(dark: dark),
             ],
           ),
         ),
---
+++ lib/features/authentication/screens/signup/widgets/signup_form.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+import '../../../../../utils/constant/colors.dart';
+import '../../../../../utils/constant/sizes.dart';
+
+class SignupForm extends StatelessWidget {
+  const SignupForm({super.key, required this.dark});
+
+  final bool dark;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'First Name',
+                    prefixIcon: Icon(Iconsax.user),
+                  ),
+                ),
+              ),
+              SizedBox(width: CSizes.sm),
+              Expanded(
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'First Name',
+                    prefixIcon: Icon(Iconsax.user),
+                  ),
+                ),
+              ),
+            ],
+          ),
+
+          SizedBox(height: CSizes.lg),
+          TextFormField(
+            decoration: InputDecoration(
+              labelText: 'Username',
+              prefixIcon: Icon(Iconsax.user_edit),
+            ),
+          ),
+          SizedBox(height: CSizes.lg),
+          TextFormField(
+            decoration: InputDecoration(
+              labelText: 'Email',
+              prefixIcon: Icon(Iconsax.direct),
+            ),
+          ),
+          SizedBox(height: CSizes.lg),
+          TextFormField(
+            decoration: InputDecoration(
+              labelText: 'Phone Number',
+              prefixIcon: Icon(Iconsax.call),
+            ),
+          ),
+          SizedBox(height: CSizes.lg),
+          TextFormField(
+            decoration: InputDecoration(
+              labelText: 'Password',
+              suffixIcon: Icon(Iconsax.eye_slash),
+              prefixIcon: Icon(Iconsax.user),
+            ),
+          ),
+
+          SizedBox(height: CSizes.lg),
+
+          Row(
+            children: [
+              SizedBox(
+                width: 20,
+                height: 20,
+                child: Checkbox(value: true, onChanged: (value) {}),
+              ),
+              SizedBox(width: CSizes.sm),
+              Text.rich(
+                TextSpan(
+                  children: [
+                    TextSpan(
+                      text: 'I agree to the ',
+                      style: Theme.of(context).textTheme.bodySmall,
+                    ),
+                    TextSpan(
+                      text: 'privacy ',
+                      style: Theme.of(context).textTheme.bodyMedium!.apply(
+                        color: dark ? CColors.white : CColors.primary,
+                        decoration: TextDecoration.underline,
+                        decorationColor: dark ? CColors.white : CColors.primary,
+                      ),
+                    ),
+                    TextSpan(
+                      text: 'and ',
+                      style: Theme.of(context).textTheme.bodySmall,
+                    ),
+                    TextSpan(
+                      text: 'Terms of us',
+                      style: Theme.of(context).textTheme.bodyMedium!.apply(
+                        color: dark ? CColors.white : CColors.primary,
+                        decoration: TextDecoration.underline,
+                        decorationColor: dark ? CColors.white : CColors.primary,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: CSizes.lg),
+
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Text("Create Account"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
